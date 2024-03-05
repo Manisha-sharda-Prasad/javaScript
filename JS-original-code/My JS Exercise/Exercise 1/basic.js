b93cac39bd8ef9f0b4e79644001b47d950c9029f
Manisha-sharda-Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-03-04 21:46:46 -0800

    Restruture

---
+++ JS-original-code/My JS Exercise/Exercise 1/basic.js
@@ -0,0 +1,17 @@
+console.log("hello manisha");
+
+a = 2;
+b = 4; // assign number
+firstname = "Manisha"; // assign string
+isSunday = false; // assign boolean
+
+aa = firstname;
+console.log(aa); // assign another variable
+
+c = a + b;
+console.log(c); // + operator, assinm calculation
+
+c = "manisha";
+console.log(c); // override prvois value.
+
+console.log(c, 2, "string-value", true); //log
