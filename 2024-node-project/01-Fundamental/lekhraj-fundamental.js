0ef67b1fd2b39dc4a32f24cf0f01df78e7fb2041
Manisha Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-03-08 22:12:26 -0800

    Functions fundamentals

---
+++ 2024-node-project/01-Fundamental/lekhraj-fundamental.js
@@ -0,0 +1,45 @@
+/*
+
+JS - 2 element : 
+1. variable
+2. function
+---------------
+
+*** Declare and then use
+
+---------------
+
+VARIABLE
+============
+-- Step -1 :: Declare
+var a;
+let b ;
+const c ;
+
+--- Step :: use (RUN)
+ a = 12;  -- use : assign 
+ b = "Lekhraj" -- use : assign 
+console.log(b) -- use : read and print 
+
+
+Function
+============
+--- Step -1 :: Declare
+
+line of code/s - group, then USE multiple times.
+function logger(){
+    // write line of code/s , Any code whatever you have learnt soo far.
+    console.log(" any code, hello world")
+    let n1 =10;
+    let n2=20;
+    let sum = n1 + n2;
+    console.log(sum)
+}
+
+
+--- Step :: USE - call, invoke, run
+logger();
+logger();  // re-use-1
+logger();
+
+*/
