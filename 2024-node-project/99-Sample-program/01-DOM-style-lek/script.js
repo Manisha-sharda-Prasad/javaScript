ce1e99aedc83476f5472695a408966dfc678253a
Manisha Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-03-19 22:03:06 -0700

    call-back- function

--- 2024-node-project/99-Sample-program/01-DOM-style-lek/script.js
+++ 2024-node-project/99-Sample-program/01-DOM-style-lek/script.js
@@ -17,3 +17,30 @@ function removeStyle() {
   div1.classList.remove('border-1');
   div1.classList.remove('bg-light');
 }
+
+// ===== First class Functon ======= CONCEPT =======
+// passing "function declartion/value" as argumnet
+// return function from function
+
+function parentFunction(c1, c2) {
+  c1();
+  console.log('hello');
+  c2();
+}
+
+const child_f1 = function () {
+  console.log('iam f2 function 1');
+  console.log('iam f2 function 2');
+  console.log('iam f2 function 3');
+  //5 min
+};
+
+const child_f2 = function () {
+  console.log('iam f3 function 1');
+  console.log('iam f3 function 2');
+  console.log('iam f3 function 3');
+  // 2 min
+};
+//======================
+
+parentFunction(child_f1, child_f2);
