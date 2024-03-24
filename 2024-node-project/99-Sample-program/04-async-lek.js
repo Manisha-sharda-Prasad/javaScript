9fd4bd0a9244d848d667f95d379dff82761c7fbc
Manisha Prasad <114974425+Manisha-sharda-Prasad@users.noreply.github.com>
2024-03-24 13:06:10 -0700

    Update 04-async-lek.js

--- 2024-node-project/99-Sample-program/04-async-lek.js
+++ 2024-node-project/99-Sample-program/04-async-lek.js
@@ -3,7 +3,7 @@ function fetchData() {
     setTimeout(() => {
       console.log('Sync TAsk after 5 minutes');
       resolve('COMPLETED');
-    }, 5000);
+    }, 3000);
   };
 
   const cb = (resolve, reject) => {
@@ -14,3 +14,29 @@ function fetchData() {
 }
 
 fetchData().then(d => console.log('fulfilled ! received data :', d));
+
+// ===========
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const fun_add = (x, y) => x + y;
+const fun_divide = (x, y) => x / y;
+const fun_subtract = (x, y) => x - y;
+const fun_multiply = (x, y) => x * y;
+
+/// >>>>>>
+function operation(n1, n2, fun) {
+  // >>>>>>  num , num , function(arg1, arg2)
+  console.log(fun(n1, n2));
+}
+
+// let result = add(10, 20);
+fun_add(10, 20);
+fun_divide(10 / 20);
+
+operation(10, 20, fun_add);
+operation(10, 20, fun_divide);
+operation(10, 20, fun_subtract);
+operation(10, 20, fun_multiply);
